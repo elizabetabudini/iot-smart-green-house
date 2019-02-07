9e675dc6c4e645a41ef77ec848ca360deb2d702a
elizabetabudini <elizabeta.budini@studio.unibo.it>
2019-02-07 21:13:04 +0100

    funziona accensione spegnimento bluetooth

--- src/sgh-mobile-app/sghapp/app/src/main/java/com/example/utente/sgh_app/MainActivity.java
+++ src/sgh-mobile-app/sghapp/app/src/main/java/com/example/utente/sgh_app/MainActivity.java
@@ -5,20 +5,21 @@ import android.bluetooth.BluetoothDevice;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import android.widget.Toast;
 import android.content.Intent;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView statusBluetooth;
-    TextView paired;
-    ImageView iconBT;
-    Button accendiBluetooth, spegniBluetooth, ricercaDispositivi, dispositiviAccopiati;
+    TextView tv_statusBluetooth, tv_elenco_dispositivi;
+    ImageView iv_iconBT;
+    Button btn_ricercaDispositivi;
+    Switch switch_bluetooth;
 
     BluetoothAdapter btAdapter;
     private static final int REQUEST_ENABLE_BT = 0;
@@ -30,92 +31,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        statusBluetooth = findViewById(R.id.statusBluetooth);
-        paired = findViewById(R.id.paired);
-        accendiBluetooth = findViewById(R.id.BluetoothON);
-        spegniBluetooth = findViewById(R.id.bluetoothOFF);
-        dispositiviAccopiati = findViewById(R.id.pairedBluetooth);
-        ricercaDispositivi = findViewById(R.id.discover);
-        iconBT = findViewById(R.id.imageBluetooth);
+        tv_statusBluetooth = findViewById(R.id.switchBluetooth);
+        tv_elenco_dispositivi = findViewById(R.id.tv_elenco_dispositivi);
+        btn_ricercaDispositivi = findViewById(R.id.btn_ricerca);
+        iv_iconBT = findViewById(R.id.imageBluetooth);
+        switch_bluetooth= findViewById(R.id.switchBluetooth);
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        //check if bluetooth is available
-
-        if(btAdapter == null){
-            statusBluetooth.setText("Bluetooth non disponibile");
-        } /*else {
-            statusBluetooth.setText("Bluetooth disponibile");
-        }*/
+        if (btAdapter == null) {
+            showToast("Questo dispositivo non supporta il bluetooth");
+        }
 
         //set image bluetooth
         if(btAdapter.isEnabled()){
-            iconBT.setImageResource(R.drawable.ic_action_on);
+            iv_iconBT.setImageResource(R.drawable.ic_action_on);
+            switch_bluetooth.setChecked(true);
+            switch_bluetooth.setText(R.string.switch_status_on);
 
         } else {
-            iconBT.setImageResource(R.drawable.ic_action_off);
+            iv_iconBT.setImageResource(R.drawable.ic_action_off);
+            switch_bluetooth.setChecked(false);
+            switch_bluetooth.setText(R.string.switch_status_off);
         }
 
-        //ON button click
-        accendiBluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!btAdapter.isEnabled()){
-                    showToast("Accendo il bluetooth");
-                    //intent che accende
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(intent, REQUEST_ENABLE_BT);
-                } else {
-                    showToast("Bluetooth già acceso");
+        switch_bluetooth.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    if(!btAdapter.isEnabled()){
+                        showToast("Accendo il bluetooth");
+                        //intent che accende
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(intent, REQUEST_ENABLE_BT);
+                    } else {
+                        showToast("Bluetooth già acceso");
+                    }
+                }else{
+                    if(btAdapter.isEnabled()){
+                        btAdapter.disable();
+                        showToast("Bluetooth spento");
+                        iv_iconBT.setImageResource(R.drawable.ic_action_off);
+                        switch_bluetooth.setText(R.string.switch_status_off);
+                    } else {
+                        showToast("Il bluetooth è già spento");
+                    }
+
                 }
             }
         });
 
-        //OFF button click
-        spegniBluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(btAdapter.isEnabled()){
-                    btAdapter.disable();
-                    showToast("Bluetooth spento");
-                    iconBT.setImageResource(R.drawable.ic_action_off);
-                } else {
-                    showToast("Il bluetooth è già spento");
-                }
-            }
-        });
-
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
         //discover button click
-        ricercaDispositivi.setOnClickListener(new View.OnClickListener() {
+        btn_ricercaDispositivi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if(!btAdapter.isDiscovering()){
                     showToast("Inizio ricerca");
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+                    //startActivityForResult(intent, REQU);
+                    btAdapter.getScanMode();
                 }
 
             }
         });
 
-        //paired button click
-        dispositiviAccopiati.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(btAdapter.isEnabled()){
-                    Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
-
-                    // Explicit Intent to launch the connect activity
-                    intent = new Intent(MainActivity.this, connect.class);
-
-                    startActivity(intent);
-
-                } else {
-                    //il bluetooth è spento quindi non può visualizza i dispositivi accoppiati
-                    showToast("Accendi il bluetooth per visualizzare i dispositivi accoppiati");
-                }
-            }
-        });
     }
 
     @Override
@@ -124,8 +103,9 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_ENABLE_BT:
                 if(resultCode == RESULT_OK){
                     //bluetooth is on
-                    iconBT.setImageResource(R.drawable.ic_action_on);
+                    iv_iconBT.setImageResource(R.drawable.ic_action_on);
                     showToast("Bluetooth acceso");
+                    switch_bluetooth.setText(R.string.switch_status_on);
                 } else {
                     //l'utente non ha acconsentito ad accendere il bluetooth
                     showToast("Non è stato possibile accendere il bluetooth");
