a49e8878600f2f9944d450defbcdf48794595712
batikus <batikus@gmail.com>
2019-03-03 15:02:35 +0100

    remove useless files

--- src/sgh-server/src/main/Progetto3/BasicController.java
+++
@@ -1,9 +0,0 @@
-package Progetto3;
-
-
-public abstract class BasicController extends Thread {
-
-	protected void waitFor(long ms) throws InterruptedException{
-		Thread.sleep(ms);
-	}
-}
--- src/sgh-server/src/main/Progetto3/Handler.java
+++
@@ -1,6 +0,0 @@
-package Progetto3;
-
-
-public interface Handler {
-	void handle(Event ev);
-}
